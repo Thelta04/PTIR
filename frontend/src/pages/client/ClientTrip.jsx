70a89617072fe3af5af7362b24466c9f67a5153d
fc62214 <mrcomedy014@gmail.com>
2026-06-03 13:06:12 +0100

    so quando o invoice e enviado e que pode fazer rating

--- frontend/src/pages/client/ClientTrip.jsx
+++ frontend/src/pages/client/ClientTrip.jsx
@@ -716,7 +716,6 @@ export default function ClientTrip() {
             <button 
               onClick={() => {
                 setIsPaidPanelClosed(true);
-                setIsRatingModalOpen(true);
               }}
               aria-label="Fechar"
               style={{
